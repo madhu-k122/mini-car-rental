c0cce00de6f59dc469b0c6548653efbbb369587b
Madhulatha <kmadhulatha8@gmail.com>
2025-10-24 11:22:24 +0530

    encrypt file created

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "🧩 Fixing Laravel permissions..."
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
+echo "🚀 Starting Apache..."
+apache2-foreground
